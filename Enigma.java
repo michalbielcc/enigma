7e8e4a9464f198297fdf0b5dba5cbe5ed3166ba3
Michal Biel <mierwew@gmail.com>
2018-12-10 13:43:45 +0100

    added methods to append input to cipher and parameters respectively

--- Enigma.java
+++ Enigma.java
@@ -1,22 +1,41 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 class Enigma {
 
-    private static ArrayList<String> readData(String[] args) {
+
+    private static ArrayList<String> readData(String[] input) {
 
         ArrayList<String> data = new ArrayList<String>();
-        for (String argument : args) {
-            data.add(argument);
+        Collections.addAll(data, input);
+        if (data.size() == 0) {
+            System.out.println("Missing input/parameter, try again");
+            System.exit(0);
         }
         return data;
     }
 
 
+    private static ArrayList<String> getParameters(ArrayList<String> data) {
+
+        ArrayList<String> parameters = new ArrayList<String>(data.subList(1, 3));
+        return parameters;
+    }
+
+
+    private static String toCipher(ArrayList<String> data) {
+
+        return data.get(0);
+    }
+
+
     public static void main(String[] args) throws IOException {
 
-        System.out.println(readData(args));
+        readData(args);
+        System.out.println(getParameters(readData(args)));
+        System.out.println(toCipher(readData(args)));
     }
     
 }
